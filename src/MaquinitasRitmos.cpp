032ef0175ff1fc8aae093223d726c9c6f4baddbe
montoyamoraga <montoyamoraga@gmail.com>
2021-07-08 16:37:36 -0400

    add draft

---
+++ src/MaquinitasRitmos.cpp
@@ -0,0 +1,14 @@
+#include "MaquinitasRitmos.h"
+
+/// @brief constructor
+MaquinitasRitmos::MaquinitasRitmos() {
+
+}
+
+void MaquinitasRitmos::setTempo(float newTempo) {
+  tempo = newTempo;
+}
+
+float MaquinitasRitmos::getTempo() {
+  return tempo;
+}
---
+++ src/MaquinitasRitmos.h
@@ -0,0 +1,17 @@
+#ifndef MAQUINITAS_RITMOS_H
+#define MAQUINITAS_RITMOS_H
+
+class MaquinitasRitmos {
+  public:
+    MaquinitasRitmos() {
+    
+  }
+
+  void setTempo(float newTempo);
+  float getTempo();
+
+  private:
+    float tempo;
+};
+
+#endif
